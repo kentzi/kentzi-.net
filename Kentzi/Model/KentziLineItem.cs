04bb734fe68ce206ff7678772f6d213ca792c67c
lalongooo <hdez.jeduardo@gmail.com>
2015-03-18 13:26:08 -0600

    Changed the KentziLineItem properties declaration

--- Kentzi/Model/KentziLineItem.cs
+++ Kentzi/Model/KentziLineItem.cs
@@ -8,47 +8,11 @@ namespace Kentzi.Model
 {
     public class KentziLineItem
     {
-        private string code;
-        public string Code
-        {
-            get { return code; }
-            set { code = value; }
-        }
-
-        private string name;
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        private string description;
-        public string Description
-        {
-            get { return description; }
-            set { description = value; }
-        }
-
-        private double price;
-        public double Price
-        {
-            get { return price; }
-            set { price = value; }
-        }
-
-        private double tax;
-        public double Tax
-        {
-            get { return tax; }
-            set { tax = value; }
-        }
-
-        private int quantity;
-        public int Quantity
-        {
-            get { return quantity; }
-            set { quantity = value; }
-        }        
-                
+        private string Code { get; set; }
+        private string Name { get; set; }
+        private string Description { get; set; }
+        private double Price { get; set; }
+        private double Tax { get; set; }
+        private int Quantity { get; set; }                
     }
 }
